755e71ec34674dba7692ed96f7de20a2c36c4b3a
root <root@kannt.ddns.net>
2024-07-19 08:19:51 +0700

    Update GameWay server

--- GameWay/Relay/RelaySetting/Task/WeeklyBackupBR.lua
+++ GameWay/Relay/RelaySetting/Task/WeeklyBackupBR.lua
@@ -1,12 +1,12 @@
 -- “ª÷‹“ª¥Œ±∏∑›’Ω≥°≈≈––
 
 function TaskShedule()
-	TaskName("WeeklyBackupBR");	
-	
+	TaskName("WeeklyBackupBR");
+
 	local h = tonumber(date("%H"))
-	
+
 	-- 60∑÷÷”¥•∑¢“ª¥Œ
-	TaskInterval(60)	
+	TaskInterval(60)
 	TaskTime(21, 1)
 	-- ÷¥––Œﬁœﬁ¥Œ
 	TaskCountLimit(0)
@@ -18,6 +18,7 @@ function TaskContent()
 	local h = tonumber(date("%H"))
 
 	if w == 6 and h == 21 then
+
 		CalcHonorPoint(5)
 		OutputMsg("ßang thËng k’ x’p hπng chi’n tr≠Íng!...")
 	end
